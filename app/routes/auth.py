8737b5cbcd161c737a81db809ea150d9a94ade7c
Siddh Mehta <siddh.m@acheron-tech.com>
2025-03-12 13:40:05 +0530

    Initial commit

---
+++ app/routes/auth.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, HTTPException, Depends
+from datetime import timedelta
+from app.schemas import UserCreate,UserResponse
+from app.database import get_database
+from app.security import get_password_hash , verify_password , create_access_token
+router = APIRouter()
+
+@router.post("/register")
+async def register(user:userCreate , db = Depends(get_database)):
+    existing_user = await db.users.find_one({"username":user.username})
+    if existing_user:
+        raise HTTPException(status_code = 400 , detail = "User already exists")
+    
+    hashed_password = get_password_hash(user.password)
+    user_dict = user.model_dump()
+    user_dict["password"] = hashed_password
+    user_dict["role"] = "user"
+    
+    await db.users.insert_one(user_dict)
+    return {"message":"user registered successfully"}
+    
+@router.post("/login")
+async def login(username:str , password: str,db = Depends(get_database)):
+    user = await db.users.find_one({"username":username})
+    if not user or not verify_password(password , user["password"]):
+        raise HTTPException(status_code = 400 , detail= "Invalid Credentials")
+    
+    access_token = create_access_token(data={"sub":username , "role":user["role"]})
+    
+    return {"Access_token":access_token , "token_type":"bearer"}
+    
+    
