daf759b26cd8b9654bbb4f2b6711856e89fd4b7f
siddhm11 <mehtasiddh011@gmail.com>
2025-03-15 17:21:51 +0530

    small changes

--- app/models.py
+++ app/models.py
@@ -13,6 +13,17 @@ from bson import ObjectId
 #this is to get the object id from the mongodb object and 
 #we are using this to return or use object id 
 
+class PyObjectId(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+        
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid ObjectId")
+        return str(ObjectId(v))
+
 class Movie(BaseModel):
     id: Optional[str]
     title:str
@@ -21,6 +32,6 @@ class Movie(BaseModel):
     genre: str
     
     class Config:
-        json_encoders = {ObjectId: StopIteration}
+        json_encoders = {ObjectId: str}
 
 
