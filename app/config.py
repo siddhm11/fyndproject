fba58dcb39e363bba686e4eb7bcff6c1fda16052
Siddh Mehta <siddh.m@acheron-tech.com>
2025-03-18 20:05:53 +0530

    This is the code , i need to see if it works

--- app/config.py
+++ app/config.py
@@ -12,3 +12,4 @@ DB_NAME = os.getenv("DB_NAME", "movie_db")
 SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+
--- app/database.py
+++ app/database.py
@@ -1,14 +1,22 @@
-# app/config.py
+
+from motor.motor_asyncio import AsyncIOMotorClient #enables async operations 
+#for fast api and mongodb since both follow asyncthings 
+
+from fastapi import FastAPI
 import os
-from dotenv import load_dotenv
+print("Connecting to MongoDB at:", os.getenv("MONGO_URL"))
 
-load_dotenv()
+#this is for mongodb connection 
 
-# MongoDB settings
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-DB_NAME = os.getenv("DB_NAME", "movie_db")
+MONGO_URL = os.getenv("MONGO_URL","mongodb://localhost:27017")
 
-# Auth settings
-SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+DB_NAME = "movie_db" # this is the data base in the mongodb instance 
+# movies and citations and stuff like that is called as a collection over here 
+
+
+client = AsyncIOMotorClient(MONGO_URL)
+
+database = client[DB_NAME]
+
+def get_database():
+    return database
